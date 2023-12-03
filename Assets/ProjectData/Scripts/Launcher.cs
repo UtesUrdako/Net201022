69ee0e12d453d6facd2f5c20b2f2519a68402d3e
EddanielMoonkin <eddanielmoonkin@gmail.com>
2023-12-03 12:00:25 +0300

    lesson3 task

--- Assets/ProjectData/Scripts/Launcher.cs
+++ Assets/ProjectData/Scripts/Launcher.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    [SerializeField] 
+    private Button _connectButton;
+
+    [SerializeField]
+    private TMP_Text _buttonText;
+
     private void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
@@ -10,17 +19,49 @@ public class Launcher : MonoBehaviourPunCallbacks
 
     private void Start()
     {
-        Connect();
+        _buttonText.text = "Disconnected";
+        _connectButton.GetComponent<Image>().color = Color.red;
+        //_connectButton.onClick.AddListener(Connect);
+        _connectButton.onClick.AddListener(ChangeConnection);
     }
 
-    private void Connect()
+    /*private void Connect()
     {
         if (PhotonNetwork.IsConnected)
             return;
-        
+
         PhotonNetwork.ConnectUsingSettings();
         PhotonNetwork.GameVersion = Application.version;
-                  
+        _connectButton.onClick.RemoveAllListeners();
+        _buttonText.text = "Connected";
+        _connectButton.GetComponent<Image>().color = Color.green;
+        _connectButton.onClick.AddListener(Disconnect);        
+    }
+
+    private void Disconnect()
+    {
+        PhotonNetwork.Disconnect();
+        _connectButton.onClick.RemoveAllListeners();
+        _buttonText.text = "Disconnected";
+        _connectButton.onClick.AddListener(Connect);
+        _connectButton.GetComponent<Image>().color = Color.red;
+    }*/
+
+    private void ChangeConnection()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Disconnect();
+            _buttonText.text = "Disconnected";
+            _connectButton.GetComponent<Image>().color = Color.red;
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = Application.version;
+            _buttonText.text = "Connected";
+            _connectButton.GetComponent<Image>().color = Color.green;
+        }         
     }
 
     public override void OnConnectedToMaster()
@@ -32,5 +73,11 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("OnJoinedRoom");
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("You have been disconnected");
+
     }
 }
--- Assets/ProjectData/Scripts/PlayFabLogin.cs
+++ Assets/ProjectData/Scripts/PlayFabLogin.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class PlayFabLogin : MonoBehaviour
 {
+    [SerializeField]
+    private Button _loginButton;
+    [SerializeField]
+    private TMP_Text _loginResultText;
+
     private void Start()
     {
-        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
-            PlayFabSettings.staticSettings.TitleId = "F6970";
+        _loginButton.onClick.AddListener(LogIn);
 
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+            PlayFabSettings.staticSettings.TitleId = "F6970";       
+
+    }
+
+    private void LogIn()
+    {
         var request = new LoginWithCustomIDRequest
         {
             CustomId = "Player 1",
@@ -16,17 +30,20 @@ public class PlayFabLogin : MonoBehaviour
         };
 
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginError);
-
     }
-    
+
     private void OnLoginSuccess(LoginResult result)
     {
         Debug.Log("Login completed");
+        _loginResultText.color = Color.green;
+        _loginResultText.text = "You have successfully logged in!";
     }
 
     private void OnLoginError(PlayFabError error)
     {
         var errorMessage = error.GenerateErrorReport();
         Debug.LogError(errorMessage);
+        _loginResultText.color = Color.red;
+        _loginResultText.text =  $"Login to account was interrupted due to an error: {errorMessage}";
     }
 }
